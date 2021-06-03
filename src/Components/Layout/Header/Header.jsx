0613476e8e95b2a75f5dbd838f578ca02c6aa4a5
hamed zabihi <hamed0zabihi@gmail.com>
2021-06-03 17:13:42 +0430

    :lipstick:enable sticky class for nav

--- src/Components/Layout/Header/Header.jsx
+++ src/Components/Layout/Header/Header.jsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from "react";
 import Logo from "./Logo";
 import MainMenu from "./MainMenu";
 import MenuRight from "./MenuRight";
 
 const Header = () => {
+  const [sticky, setsticky] = useState(false);
+
+  const handleSticky = () => {
+    const offset = window.scrollY;
+    if (offset < 245) {
+      setsticky(true);
+    } else {
+      setsticky(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleSticky);
+  }, []);
+  if (sticky) {
+    console.log("stiky", sticky);
+  }
   return (
     <>
       <header>
         <div className="header-area">
-          <div className="main-header header-sticky">
+          <div
+            className={
+              sticky
+                ? "main-header header-sticky"
+                : "main-header header-sticky sticky-bar sticky"
+            }
+          >
             <div className="container-fluid">
               <div className="menu-wrapper">
                 <Logo />
