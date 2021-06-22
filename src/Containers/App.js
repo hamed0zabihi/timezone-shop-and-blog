109101298c623d0ad89f81e920fa65e12bfc5bc8
hamed zabihi <hamed0zabihi@gmail.com>
2021-06-22 23:27:29 +0430

    :racehorse:ScrollToTop when routing change

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "../Components/Utils/ScrollToTop";
 
 import TimeZone from "./TimeZone";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <TimeZone />
     </BrowserRouter>
   );
