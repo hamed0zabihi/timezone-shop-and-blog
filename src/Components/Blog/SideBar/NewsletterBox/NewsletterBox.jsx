5cdee491d38ad8316b0863f36d9249b6efc0f95d
hamed zabihi <hamed0zabihi@gmail.com>
2021-07-06 18:04:49 +0430

    :zap: use Formik in NewsLetterBox

--- src/Components/Blog/SideBar/NewsletterBox/NewsletterBox.jsx
+++ src/Components/Blog/SideBar/NewsletterBox/NewsletterBox.jsx
@@ -1,26 +1,36 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 const NewsletterBox = () => {
   return (
     <aside className="single_sidebar_widget newsletter_widget">
       <h4 className="widget_title">Newsletter</h4>
-
-      <form action="#">
-        <div className="form-group">
-          <input
-            type="email"
-            className="form-control"
-            onfocus="this.placeholder = ''"
-            onblur="this.placeholder = 'Enter email'"
-            placeholder="Enter email"
-            required
-          />
-        </div>
-        <button
-          className="button rounded-0 primary-bg text-white w-100 btn_1 boxed-btn"
-          type="submit"
-        >
-          Subscribe
-        </button>
-      </form>
+      <Formik
+        initialValues={{
+          email: "",
+        }}
+        validationSchema={Yup.object({
+          email: Yup.string().email().required("required"),
+        })}
+      >
+        <Form>
+          <div className="form-group">
+            <Field
+              name="email"
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+            />
+          </div>
+          <button
+            className="button rounded-0 primary-bg text-white w-100 btn_1 boxed-btn"
+            type="submit"
+          >
+            Subscribe
+          </button>
+          <ErrorMessage component="span" name="email" className="text-danger" />
+        </Form>
+      </Formik>
     </aside>
   );
 };
