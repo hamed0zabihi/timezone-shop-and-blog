6f482c6baa303bde47b9252016b9e9bcd246baf7
hamed zabihi <hamed0zabihi@gmail.com>
2021-07-06 23:14:54 +0430

    :zap: dynamic category list and count number  in CategoryBox

--- src/Components/Blog/SideBar/CategoryBox/CategoryBox.jsx
+++ src/Components/Blog/SideBar/CategoryBox/CategoryBox.jsx
@@ -1,44 +1,34 @@
+import { useSelector } from "react-redux";
+
 const CategoryBox = () => {
+  const AllArticles1 = useSelector((state) => state.articles);
+
+  const AllCategoryInOneArray = AllArticles1.map((el) => el.category).flat(1);
+  let uniqueCategories = [...new Set(AllCategoryInOneArray)];
+  const p = uniqueCategories.reduce((acc, category) => {
+    return [
+      ...acc,
+      {
+        category,
+        productdat: AllArticles1.filter((product) =>
+          product.category.find((cat) => cat === category)
+        ),
+      },
+    ];
+  }, []);
+  console.log("p", p);
   return (
     <aside className="single_sidebar_widget post_category_widget">
       <h4 className="widget_title">Category</h4>
       <ul className="list cat-list">
-        <li>
-          <a href="/#" className="d-flex">
-            <p>Resaurant food</p>
-            <p>(37)</p>
-          </a>
-        </li>
-        <li>
-          <a href="/#" className="d-flex">
-            <p>Travel news</p>
-            <p>(10)</p>
-          </a>
-        </li>
-        <li>
-          <a href="/#" className="d-flex">
-            <p>Modern technology</p>
-            <p>(03)</p>
-          </a>
-        </li>
-        <li>
-          <a href="/#" className="d-flex">
-            <p>Product</p>
-            <p>(11)</p>
-          </a>
-        </li>
-        <li>
-          <a href="/#" className="d-flex">
-            <p>Inspiration</p>
-            <p>21</p>
-          </a>
-        </li>
-        <li>
-          <a href="/#" className="d-flex">
-            <p>Health Care (21)</p>
-            <p>09</p>
-          </a>
-        </li>
+        {p.map((elb, index) => (
+          <li key={index}>
+            <a href="/#" className="d-flex">
+              <p>{elb.category}</p>
+              <p>({elb.productdat.length})</p>
+            </a>
+          </li>
+        ))}
       </ul>
     </aside>
   );
