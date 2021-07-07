e372cce1ac2d86abae7b85d3854fc7cf66f14e85
hamed zabihi <hamed0zabihi@gmail.com>
2021-07-07 21:14:23 +0430

    :zap: five recent post from articles Reduc state

--- src/Components/Blog/SideBar/RecentPostBox/RecentPostBox.jsx
+++ src/Components/Blog/SideBar/RecentPostBox/RecentPostBox.jsx
@@ -1,43 +1,30 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import _ from "lodash";
+
 const RecentPostBox = () => {
+  const articles = useSelector((state) => state.articles);
+  //select 5 recent post
+  const fiveRecentArticle = _(articles)
+    .orderBy("createdAt", "desc")
+    .take(5)
+    .value();
+  //date
+
   return (
     <aside className="single_sidebar_widget popular_post_widget">
       <h3 className="widget_title">Recent Post</h3>
-      <div className="media post_item">
-        <img src="/images/post/post_1.png" alt="post" />
-        <div className="media-body">
-          <a href="single-blog.html">
-            <h3>From life was you fish...</h3>
-          </a>
-          <p>January 12, 2019</p>
+      {fiveRecentArticle.map((el, index) => (
+        <div className="media post_item" key={index}>
+          <img src={el.image} alt="post" width="80px" height="80px" />
+          <div className="media-body">
+            <Link to={`/blog/${el.id}`}>
+              <h3>{el.title.slice(0, 20)}...</h3>
+            </Link>
+            <p>January 12, 2019</p>
+          </div>
         </div>
-      </div>
-      <div className="media post_item">
-        <img src="/images/post/post_2.png" alt="post" />
-        <div className="media-body">
-          <a href="single-blog.html">
-            <h3>The Amazing Hubble</h3>
-          </a>
-          <p>02 Hours ago</p>
-        </div>
-      </div>
-      <div className="media post_item">
-        <img src="/images/post/post_3.png" alt="post" />
-        <div className="media-body">
-          <a href="single-blog.html">
-            <h3>Astronomy Or Astrology</h3>
-          </a>
-          <p>03 Hours ago</p>
-        </div>
-      </div>
-      <div className="media post_item">
-        <img src="/images/post/post_4.png" alt="post" />
-        <div className="media-body">
-          <a href="single-blog.html">
-            <h3>Asteroids telescope</h3>
-          </a>
-          <p>01 Hours ago</p>
-        </div>
-      </div>
+      ))}
     </aside>
   );
 };
