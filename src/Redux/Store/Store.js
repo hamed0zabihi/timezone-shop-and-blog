6924fb7d6edfdf59c48768a2b71aa293ca865824
hamed zabihi <hamed0zabihi@gmail.com>
2021-07-15 10:55:44 +0430

    :zap:Disable temporary redux dev tools

--- src/Redux/Store/Store.js
+++ src/Redux/Store/Store.js
@@ -3,13 +3,11 @@ import thunk from "redux-thunk";
 import { GetArticles } from "../Actions/Blog/Articles/GetArticles";
 import { reducers } from "../Reducers";
 
-export const Store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+//for reduxDevTools Extesion Browser
+// const reduxDevTools =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+export const Store = createStore(reducers, compose(applyMiddleware(thunk)));
 //
 Store.dispatch(GetArticles());
 //subscribe
